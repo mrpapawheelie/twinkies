52aa72728c633a19b603436d8b33be07f24f3c96
PapaWheelie <86974690+mrpapawheelie@users.noreply.github.com>
2023-07-07 14:22:10 -0700

    Update layout.tsx
    Add 100 vw vh since the stars are random and may create scrollbars.

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className='w-screen w-height'>
       <body className={inter.className}>
       <Twinkies />
       {children}
